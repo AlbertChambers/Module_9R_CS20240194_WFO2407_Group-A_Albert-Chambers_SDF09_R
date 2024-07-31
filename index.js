b02c05eb643581b980d84f2e8baf6e5c08611f4d
Albert Chambers <albertchambers09@gmail.com>
2024-07-31 09:02:51 +0200

    if / else if created

--- index.js
+++ index.js
@@ -5,7 +5,13 @@
 
 let firstCard = 6
 let secondCard = 9
-
 let sum = firstCard + secondCard
-
 console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
+} else if (sum > 21) {
+    console.log("You're out of the game!")
+}
