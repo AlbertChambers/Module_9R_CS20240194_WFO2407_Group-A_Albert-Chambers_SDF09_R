c7cb02f13c941cf7060c536e353ca1fbdb772bbe
Albert Chambers <albertchambers09@gmail.com>
2024-07-31 09:40:56 +0200

    declare and reasign message

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
-console.log(sum)
+let firstCard = 6;
+let secondCard = 9;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = ""
+console.log(sum);
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")
-} else if (sum > 21) {
-    console.log("You're out of the game!")
-}
+   message = "Wohoo! You've got Blackjack!"
+    hasBlackjack = true
+} else {
+    message = "You're out of the game!"
+    isAlive = false
+};
+
+// cash out
+console.log(hasBlackjack);
